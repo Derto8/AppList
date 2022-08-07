6ffd68e08b58d2c546791cede098719b2a75a6ff
Derto8 <kolya.polozov@inbox.ru>
2022-08-07 16:56:03 +0400

    AppList

---
+++ ListApps/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ListApps.Models;
+using System.ComponentModel;
+using ListApps.Services;
+using System.Data;
+
+namespace ListApps
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string Path = $"{Environment.CurrentDirectory}\\appDataList.json";
+        private BindingList<AppModel> _appDataList;
+        private FileIOServices _fileIoServices;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _fileIoServices = new FileIOServices(Path);
+            try
+            {
+                _appDataList = _fileIoServices.LoadData();
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка: {ex.Message}");
+                Close();
+            }
+
+
+            dgApps.ItemsSource = _appDataList;
+            _appDataList.ListChanged += _appDataList_ListChanged;
+        }
+
+        private void _appDataList_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            if(e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemChanged)
+            {
+                try
+                {
+                    _fileIoServices.SaveData(sender);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Произошла ошибка: {ex.Message}");
+                    Close();
+                }
+
+            }
+        }
+    }
+}
---
+++ ListApps/Services/FileIOServices.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+using System.ComponentModel;
+using ListApps.Models;
+
+namespace ListApps.Services
+{
+    internal class FileIOServices
+    {
+        private readonly string Path;
+
+        public FileIOServices(string Path)
+        {
+            this.Path = Path;
+        }
+
+        public BindingList<AppModel> LoadData()
+        {
+            var fileExists = File.Exists(Path);
+            if (!fileExists)
+            {
+                File.CreateText(Path).Dispose();
+                return new BindingList<AppModel>();
+            }
+            using (var reader = File.OpenText(Path))
+            {
+                var fileText = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<BindingList<AppModel>>(fileText);
+            }
+        }
+
+        public void SaveData(object appDataList)
+        {
+            using (StreamWriter writer = File.CreateText(Path))
+            {
+                string output = JsonConvert.SerializeObject(appDataList);
+                writer.Write(output);
+            }
+        }
+    }
+}
